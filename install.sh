ae51ddb1e22195923b6b583dee7e7b3fc4c08636
stevevoto <stevevoto@live.com>
2024-01-16 14:40:23 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,9 @@
 apt-get -y install python2
 yum -y install python2
 useradd -m webtrafficgen
-mkdir /var/lib/idp-trafficgen
-
 cp ./idp-traffgen.service /etc/systemd/system/
 sudo systemctl daemon-reload
 systemctl start idp-traffgen.service
 systemctl status idp-traffgen.service
 systemctl is-active idp-traffgen.service
 systemctl enable idp-traffgen.service
-
